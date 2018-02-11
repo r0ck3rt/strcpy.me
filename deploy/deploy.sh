e80c4815541bfd8abea880c963450b5a4c3efb19
virusdefender <virusdefender@outlook.com>
2018-02-11 23:46:41 +0800

    add ci

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-cd ..
 jekyll build
 cp deploy/.htaccess _site
-cd deploy
-source ~/blog_deploy.sh
 TARGETFOLDER='/'
-SOURCEFOLDER='../_site'
+SOURCEFOLDER='_site'
+
+HOST='cu.ftp.zeus.smartgslb.com'
+USER='ftp@b18b742f.host.smartgslb.com'
 
 lftp -f "
 open $HOST
 user $USER $PASS
-lcd $SOURCEFOLDER
-mirror --reverse --delete --verbose $SOURCEFOLDER $TARGETFOLDER
+mirror --reverse --delete --parallel=10 --verbose $SOURCEFOLDER $TARGETFOLDER
 bye
 "
